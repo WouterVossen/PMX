bda9caa3d8782150a2c196a6278f9ad228bcd23b
WouterVossen <wouter_vossen@cargill.com>
2025-08-31 16:39:26 +0200

    Update game.py

--- game.py
+++ game.py
@@ -53,8 +53,8 @@ st.subheader(f"📅 Market Day: {selected_date}")
 st.markdown("### 📈 Forward Curve")
 
 fig, ax = plt.subplots(figsize=(7, 3))  # compact
-mids = (curve_today["bid"] + curve_today["ask"]) / 2
-ax.plot(curve_today["contract"], mids, marker="o", label="Mid Price")
+#mids = (curve_today["bid"] + curve_today["ask"]) / 2
+#ax.plot(curve_today["contract"], mids, marker="o", label="Mid Price")
 ax.fill_between(curve_today["contract"], curve_today["bid"], curve_today["ask"], alpha=0.20, label="Bid/Ask Spread")
 
 for _, row in curve_today.iterrows():
